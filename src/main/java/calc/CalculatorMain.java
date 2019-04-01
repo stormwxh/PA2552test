e577ce1097237742554c4d4bb5f17ce502389b79
maike zhang <zhangmaike3044@gmail.com>
2019-04-01 20:52:44 +0200

    111

--- src/main/java/calc/CalculatorMain.java
+++ src/main/java/calc/CalculatorMain.java
@@ -7,21 +7,5 @@ public class CalculatorMain {
 	public int add(int a, int b){
 		return a+b;
 	}
-	public int sub(int a, int b) {
-		// TODO Auto-generated method stub
-		return a-b;
-	}
-	public int mul(int a, int b) {
-		// TODO Auto-generated method stub
-		return a*b;
-	}
-	public int div(int a, int b) {
-		// TODO Auto-generated method stub
-		if(b==0) {
-			return 0;
 
-		}
-		return a/b;
-
-	}
 }
